19aa06340b2867db013d19bf07555ad4c42cd006
Elizabeth <elizabethtreahy@gmail.com>
2022-04-03 19:47:17 -0400

    commit tree 8d6ea80f3a452ae201f47548117fb6f80f4be87c

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
+  const len = string.length;
+
+  // loop through half of the string
+  for (let i = 0; i < len / 2; i++) {
+
+    // check if first and last string are same
+    if (string[i] !== string[len - 1 - i]) {
+      return 'It is not a palindrome';
+    }
+  }
+  return 'It is a palindrome';
 }
+  // // take input
+  // const string = ("");
+
+  // // call the function
+  // const value = checkPalindrome(string);
+
+  // console.log(value);
+
+
 
 /* 
   Add your pseudocode here
